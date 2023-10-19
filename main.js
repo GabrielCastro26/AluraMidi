939e694e75feee50aa9fcd2c91eb592aea09ba6d
GabrielCastro26 <126919708+GabrielCastro26@users.noreply.github.com>
2023-10-19 18:01:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ while(contador <listaDeTeclas.length) {
   const instrumento = tecla.classList[1];
   const idAudio =`#som_${instrumento}`;
   tecla.onclick =function () {
-  tocaSom('#som_tecla_splash');
+  tocaSom(idAudio);
 };
   contador = contador + 1;
   console.log(contador);
