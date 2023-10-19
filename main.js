c123b6c015b4a74fad74e8b202c4ee1a26cf8685
GabrielCastro26 <126919708+GabrielCastro26@users.noreply.github.com>
2023-10-19 17:38:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador=0;
 while(contador <listaDeTeclas.length) {
   listaDeTeclas[contador].onclick =function () {
-  tocaSom(‘#som_tecla_splash’);
+  tocaSom('#som_tecla_splash');
 };
   contador = contador + 1;
   console.log(contador);
