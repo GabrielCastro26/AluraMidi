0681eead9d5262200168ee5c7f464fbe0d3542cc
GabrielCastro26 <126919708+GabrielCastro26@users.noreply.github.com>
2023-10-26 16:53:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
 for(let contador=0; contador <listaDeTeclas.length; contador++) {
   const tecla= listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
