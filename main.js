fd5bceac4e1a0b0b97461d7c584474d4cb9684ac
GabrielCastro26 <126919708+GabrielCastro26@users.noreply.github.com>
2023-10-26 17:19:05 -0300

    Update main.js

--- main.js
+++ main.js
@@ -19,8 +19,8 @@ for(let contador=0; contador <listaDeTeclas.length; contador++) {
   tocaSom(idAudio);
 };
 
-  tecla.onkeydown = function () {
-tecla.classList.add('ativa');
+  tecla.onkeydown =function () {
+  tecla.classList.add('ativa');
 }
   
   console.log(instrumento);
